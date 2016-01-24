c62ec3f799d4b6879395ed44528bff50389e7589
Richard Saul <rich_saul@yahoo.com>
2016-01-23 22:28:35 -0600

    Define cached matrix object
    The two functions define a cached matrix object and define a function
    for testing, calculating, storing, and returning the cached matrix'
    inverse matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions defined in cachematrix.R work together to define a cached matrix
+## object and to a calculate and cache the correlative inverse matrix.
 
-## Write a short comment describing this function
+## Sample matrices and their inverses, for testing and verification
+#o     <- matrix(c(3,4,5,7), nrow=2, ncol=2, byrow=TRUE)
+#o_inv <- matrix(c(7,-4,-5,3), nrow=2, ncol=2, byrow=TRUE)
+#p     <- matrix(c(1,0,0,-5,1,0,20,-4,1), nrow=3, ncol=3, byrow=TRUE)
+#p_inv <- matrix(c(1,0,0,5,1,0,0,4,1), nrow=3, ncol=3, byrow=TRUE)
+#q     <- matrix(c(1,0,0,0,1,0,0,-4,1), nrow=3, ncol=3, byrow=TRUE)
+#q_inv <- matrix(c(1,0,0,0,1,0,0,4,1), nrow=3, ncol=3, byrow=TRUE)
+#r     <- matrix(c(1,0,0,0,1,1,0,0,1,2,1,0,1,3,3,1), nrow=4, ncol=4, byrow=TRUE)
+#r_inv <- matrix(c(1,0,0,0,-1,1,0,0,1,-2,1,0,-1,3,-3,1), nrow=4, ncol=4, byrow=TRUE)
+
+
+## makeCachedMatrix defines a cached matrix object with four functions
+## makeCachedMatrix requires a matrix as a parameter -- like the sample values
+## o, p, q, and r above -- and returns an object with four functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse matrix of a cached matrix object.  If the cached
+## matrix already has an inverse, cacheSolve returns it; otherwise, cacheSolve 
+## calculates and stores the invserse to the cached matrix object using its setinv()
+## function.  cacheSolve requires a cached matrix object (output of makeCacheMatrix)
+## as an input parameter and returns the inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
